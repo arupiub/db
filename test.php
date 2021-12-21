f06eafc09265c092d41c0487b93de66735847d86
arupiub <96477154+arupiub@users.noreply.github.com>
2021-12-21 22:41:14 +0600

    Add files via upload

--- test.php
+++ test.php
@@ -4,20 +4,52 @@ $password = '';
 $database = 'db';
 $mysqli = new mysqli('localhost', $username, $password, $database);
 
-$query = "SELECT * FROM semester";
+$query = "SELECT * FROM classroom";
 echo "<b> <center>Database Output</center> </b> <br> <br>";
 
-if ($result = $mysqli->query($query)) {
-
-    while ($row = $result->fetch_assoc()) {
-        $field1name = $row["semID"];
-        $field2name = $row["semester"];
-        $field3name = $row["year"];
-
-        echo '<b>'.$field1name.$field2name.'</b><br />';
-        echo $field3name.'<br />';
-    }
+$result = $mysqli->query($query);
 
 /*freeresultset*/
-$result->free();
-}
+#$result->free();
+
+?>
+
+<html>
+  <head>
+    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+    <script type="text/javascript">
+      google.charts.load('current', {'packages':['corechart']});
+      google.charts.setOnLoadCallback(drawChart);
+
+      function drawChart() {
+
+        var data = google.visualization.arrayToDataTable([
+          ['Room No', 'Room Capacity'],
+          
+
+          <?php
+			while ($row = $result->fetch_assoc()) {
+			    $field1name = $row["roomNo"];
+			    $field2name = $row["roomCapacity"];
+			    
+
+			    echo "['".$field1name."',".$field2name."],";
+			    
+			}
+			?>
+        ]);
+
+        var options = {
+          title: 'My Daily Activities'
+        };
+
+        var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+
+        chart.draw(data, options);
+      }
+    </script>
+  </head>
+  <body>
+    <div id="piechart" style="width: 900px; height: 500px;"></div>
+  </body>
+</html>
